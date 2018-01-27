1e96f1f3202c9a7a7dce904f197c3ec5302d6ba4
Nicholas Miller <nicholas.anthony.miller@gmail.com>
2018-01-27 18:49:45 -0500

    Add comments to sklearn api

--- sk-train.py
+++ sk-train.py
@@ -17,29 +17,50 @@ from sklearn_crfsuite import metrics
 
 #http://sklearn-crfsuite.readthedocs.io/en/latest/tutorial.html
 
-TRAIN_SOURCE = './train.gold'
-DEV_SOURCE = './dev.gold'
 
-def get_tuples(training_source):
+#Location of training, dev, and test sets
+TRAIN_SOURCE = '../project1-train-dev/train.gold'
+DEV_SOURCE = '../project1-train-dev/dev.gold'
 
-    training_sents = list()
+def get_tuples(filename):
+    """Turn a gold file into lists of tuples for CRF processing
+    Inputs:
+        filename: path to gold file
+    Returns:
+        sents: list of list of tuples
+    """
+    sents = list()
 
-    with open(training_source) as source:
+    #TODO: do this with less nesting
+    with open(filename) as source:
+        #The current sentence: a list of (token, pos, label) tuples
         current_sent = list()
+
         for line in source:
+            #If it's not blank. Possibly redundant with line below.
             if len(line) > 0:
+                #Split at whitespace. Result will be [index, token, pos, label]
                 line_tokens = line.split()
-                #print(line_tokens)
-                if len(line_tokens) > 0:
-                    if line_tokens[0] == '0':
-                        if len(current_sent) > 0:
-                            training_sents.append(current_sent)
+                #print(line_tokens) #for debugging
+                if len(line_tokens) > 0: #If not a blank line
+                    if line_tokens[0] == '0': #index==0 means start of new sentence
+                        if len(current_sent) > 0: #add the current sentence to sents
+                            sents.append(current_sent)
+                        #Make a tuple of everything but the index
+                        #And add to current_sent
                         current_sent = [tuple(line_tokens[1:])]
                     else:
                         current_sent.append(tuple(line_tokens[1:]))
-    return training_sents
+    return sents
 
 def word2features(sent, i):
+    """Convert sentence to features.
+    Inputs:
+        sent: list of (word, pos, label) tuples
+        i: index of particular (word,pos,label) in the sent
+    Returns:
+        features: dict of features
+    """
     word = sent[i][0]
     postag = sent[i][1]
 
@@ -85,32 +106,73 @@ def word2features(sent, i):
 
 
 def sent2features(sent):
+    """Convert entire sentence to features
+    Inputs:
+        sent: list of (word, pos, label) tuples
+    Returns:
+        list of feature dicts
+    """
     return [word2features(sent, i) for i in range(len(sent))]
 
 def sent2labels(sent):
+    """Extract labels from sentence
+    Inputs:
+        sent: list of (word, pos, label) tuples
+    Returns:
+        list of labels
+    """
     return [label for token, postag, label in sent]
 
 def sent2tokens(sent):
+    """Extract tokens from sentence
+    Inputs:
+        sent
+    Returns:
+        list of tokens
+    """
     return [token for token, postag, label in sent]
 
 def train(X_train, y_train):
-    crf = sklearn_crfsuite.CRF(
+    """Train CRF model
+    Inputs:
+        X_train: list of feature dicts for training set
+        y_train: list of labels for training set
+    Returns:
+        model: trained CRF model
+    """
+    model = sklearn_crfsuite.CRF(
     algorithm='lbfgs',
     c1=0.1,
     c2=0.1,
     max_iterations=100,
     all_possible_transitions=True
     )
-    crf.fit(X_train, y_train)
-    return crf
+    model.fit(X_train, y_train)
+    return model
 
-def evaluate_model(crf, X_dev, y_dev):
+def evaluate_model(model, X_dev, y_dev):
+    """Evaluate the model
+    Inputs:
+        model: trained CRF model
+        X_dev: list of feature dicts for dev set
+        y_dev: list of labels for dev set
+    Returns:
+        None (prints metrics)
+    """
+
+    #Get the labels we're evaluating
     labels = list(crf.classes_)
+
+    #Most labels are 'O', so we ignore,
+    #otherwise our scores will seem higher than they actually are.
     labels.remove('O')
+
     print("Predicting labels")
     y_pred = crf.predict(X_dev)
-    print(y_pred[:10])
-    print(y_dev[:10])
+
+    #print(y_pred[:10]) #for debugging
+    #print(y_dev[:10]) #for debugging
+
     print("Displaying accuracy")
     metrics.flat_f1_score(y_dev, y_pred,
                       average='weighted', labels=labels)
@@ -125,17 +187,22 @@ def evaluate_model(crf, X_dev, y_dev):
     ))
 
 if __name__ == "__main__":
+    print("Converting gold files to sentence tuples...")
     train_sents = get_tuples(TRAIN_SOURCE)
     dev_sents = get_tuples(DEV_SOURCE)
-    print(train_sents[2])
-    print(sent2features(train_sents[0])[0])
+
+    #print(train_sents[2]) #for debugging
+    #print(sent2features(train_sents[0])[0]) #for debugging
+
+    print("Building training and dev sets...")
     X_train = [sent2features(s) for s in train_sents]
     y_train = [sent2labels(s) for s in train_sents]
-
-    print("Training model")
-    crf = train(X_train, y_train)
 
     X_dev = [sent2features(s) for s in dev_sents]
     y_dev = [sent2labels(s) for s in dev_sents]
 
+    print("Training model...")
+    crf = train(X_train, y_train)
+
+    print("Evaluating model...")
     evaluate_model(crf, X_dev, y_dev)
